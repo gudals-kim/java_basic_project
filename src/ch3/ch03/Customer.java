861f81b38c52c5b819812df6a2ffe75a25884986
kimgudals <kimtuti95@gmail.com>
2022-01-06 06:44:11 +0900

    [update] 2022-01-05 클래스 생성과정과 형변환

---
+++ src/ch3/ch03/Customer.java
@@ -0,0 +1,62 @@
+package ch3.ch03;
+
+//고객의 속성 : 고객 아이디, 고객이름, 고객등급, 보너스 포인트, 보너스 포인트, 적립비율
+// 일반 고객의 경우 물품 구매시 1% 보너스 적립
+
+
+public class Customer {
+    protected int customerID;
+    protected String customerName;
+    protected String customerGrade;
+    int bonusPoint;
+    double bonusRatio;
+
+//    public Customer() {
+//        customerGrade = "SILVER";
+//        bonusRatio = 0.01;
+//
+//        System.out.println("Customer() call");
+//    }
+
+    public Customer(int customerID,String customerName){
+        this.customerID = customerID;
+        this.customerName = customerName;
+
+        System.out.println("Customer(int, String) call");
+    }
+
+    public int calcPrice(int price) {
+        bonusPoint += price * bonusRatio;
+        return price;
+    }
+
+    public String showCustomerInFo() {
+        return customerName + "님의 등급은" + customerGrade + "이며, 보너스 포인트는" + bonusPoint + "입니다";
+    }
+
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerGrade() {
+        return customerGrade;
+    }
+
+    public void setCustomerGrade(String customerGrade) {
+        this.customerGrade = customerGrade;
+    }
+
+}
---
+++ src/ch3/ch03/CustomerTest.java
@@ -0,0 +1,13 @@
+package ch3.ch03;
+
+public class CustomerTest {
+    public static void main(String[] args) {
+        Customer customerLee = new Customer(10010,"이순신");
+        customerLee.bonusPoint = 1000;
+        System.out.println(customerLee.showCustomerInFo());
+
+        Customer customerkim = new VIPCustomer(10030,"김유신");//Customer(부모) 클래스로 생성을 해도 생성이 된다. (업케스팅) 단 이때는 변수 접근이 가능한건 customer 부분만이다.
+        customerkim.bonusPoint = 10000;
+        System.out.println(customerkim.showCustomerInFo());
+    }
+}
