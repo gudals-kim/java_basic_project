075e777dddc2e6500e24e791f15755beb4393ef2
kimgudals <kimtuti95@gmail.com>
2022-01-07 09:30:07 +0900

    [update] 2022-01-07 toString 연습

---
+++ src/ch4/ch01/BookTest.java
@@ -0,0 +1,12 @@
+package ch4.ch01;
+
+public class BookTest {
+    public static void main(String[] args) {
+        Book book = new Book("데이안","헤르만 헤세");
+        System.out.println(book);
+
+        String str = new String("test");
+        System.out.println(str);
+    }
+
+}
