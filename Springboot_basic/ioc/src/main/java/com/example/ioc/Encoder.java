c604de7f48bc492d8b9ed8960464aeb3b46af8d8
kimgudals <kimtuti95@gmail.com>
2022-01-26 23:06:00 +0900

    [update] 2022-01-26 스프링부트 - IOC/DI 개념

---
+++ Springboot_basic/ioc/src/main/java/com/example/ioc/Encoder.java
@@ -0,0 +1,13 @@
+package com.example.ioc;
+
+public class Encoder{
+    private IEncoder iEncoder;
+
+    public Encoder(IEncoder iEncoder){
+        this.iEncoder = iEncoder;
+    }
+
+    public String encode(String message){
+        return iEncoder.encode(message);
+    }
+}
