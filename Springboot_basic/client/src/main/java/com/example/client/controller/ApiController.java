0422fbfd80ec68ceaf39e88432b41a9740e61216
kimgudals <kimtuti95@gmail.com>
2022-01-28 00:06:55 +0900

    [update] 2022-01-27 스프링부트 - RestTemplate

--- Springboot_basic/client/src/main/java/com/example/client/controller/ApiController.java
+++ Springboot_basic/client/src/main/java/com/example/client/controller/ApiController.java
@@ -1,2 +1,25 @@
-package com.example.client.controller;public class ApiController {
+package com.example.client.controller;
+
+import com.example.client.service.RestTemplateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/client")
+public class ApiController {
+
+    private final RestTemplateService restTemplateService;
+
+    public ApiController(RestTemplateService restTemplateService) {
+        this.restTemplateService = restTemplateService;
+    }
+
+    @GetMapping("")
+    public String geeHello(){
+        return restTemplateService.hello();
+    }
+
+
 }
--- Springboot_basic/client/src/main/java/com/example/client/service/RestTemplateService.java
+++ Springboot_basic/client/src/main/java/com/example/client/service/RestTemplateService.java
@@ -1,2 +1,29 @@
-package com.example.client.service;public class RestTemplateService {
+package com.example.client.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Service
+public class RestTemplateService {
+
+
+    // response 응답을 받아올거임
+    public String hello(){
+        URI uri = UriComponentsBuilder // http://localhost/api/server/hello 주소생성
+                .fromUriString("http://localhost")  //http://localhost에 생성
+                .path("/api/server/hello")//경로 지정
+                .encode()// 인코딩 옵션
+                .build()
+                .toUri();
+
+        System.out.println(uri.toString());
+
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class);
+
+        return result;
+    }
 }
