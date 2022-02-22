2128e7e65ab09e3ae027c7b07732dfb1d72d7c9f
kimgudals <kimtuti95@gmail.com>
2022-02-22 23:54:44 +0900

    스프링 - 스프링으로 회원관리 앱 만들기 v1

---
+++ Springboot_basic/springMVC/myservlet/src/main/java/com/example/myservlet/web/springMVC/v1/SpringMemberListControllerV1.java
@@ -0,0 +1,23 @@
+package com.example.myservlet.web.springMVC.v1;
+
+import com.example.myservlet.domain.Member;
+import com.example.myservlet.domain.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class SpringMemberListControllerV1 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members")
+    public ModelAndView process() {
+        List<Member> members = memberRepository.findAll();
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", members);
+
+        return mv;
+    }
+}
