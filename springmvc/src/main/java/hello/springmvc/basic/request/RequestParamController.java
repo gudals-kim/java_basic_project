604540c223417b245d1671332efd235d24bcd553
kimgudals <kimtuti95@gmail.com>
2022-02-28 17:59:07 +0900

    [스프링개념] RequestBody Param

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,21 +1,38 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 @Slf4j
 @Controller
 public class RequestParamController {
 
+/*    클라이언트에서 서버로 요청 데이터를 전달할 때는 주로 다음 3가지 방법을 사용한다.
+    1. GET - 쿼리 파라미터
+           /url?username=hello&age=20
+           메시지 바디 없이, URL의 쿼리 파라미터에 데이터를 포함해서 전달
+           예) 검색, 필터, 페이징등에서 많이 사용하는 방식
+    2. POST - HTML Form
+            content-type: application/x-www-form-urlencoded
+            메시지 바디에 쿼리 파리미터 형식으로 전달 username=hello&age=20
+            예) 회원 가입, 상품 주문, HTML Form 사용
+    3. HTTP message body에 데이터를 직접 담아서 요청
+            HTTP API에서 주로 사용, JSON, XML, TEXT
+            데이터 형식은 주로 JSON 사용
+            POST, PUT, PATCH
+*/
+
+//    RequestParam 사용
     @RequestMapping("/request-param-v1")
     public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
@@ -107,5 +124,32 @@ public class RequestParamController {
                 paramMap.get("age"));
         return "ok";
     }
+//    ModelAttribute 사용
+    /**
+     *  @ModelAttribute 사용
+     * 참고: model.addAttribute(helloData) 코드도 함께 자동 적용됨, 뒤에 model을 설명할 때
+     자세히 설명
+     */
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
+    /**
+     * @ModelAttribute 생략 가능
+     * String, int 같은 단순 타입 = @RequestParam
+     * argument resolver 로 지정해둔 타입 외 = @ModelAttribute
+     */
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(),
+                helloData.getAge());
+        return "ok";
+    }
+
+
+
 }
 
